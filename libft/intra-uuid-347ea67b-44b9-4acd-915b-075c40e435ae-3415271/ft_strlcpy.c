5da3d1d2c94f7af77064ffe743be6b7aaf83f290
Jihee Kim <jihekim@c1r3s2.42seoul.kr>
2020-12-24 12:41:05 +0900

    libft

---
+++ libft/intra-uuid-347ea67b-44b9-4acd-915b-075c40e435ae-3415271/ft_strlcpy.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jihekim <jihekim@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/23 14:28:21 by jihekim           #+#    #+#             */
+/*   Updated: 2020/12/23 21:44:49 by jihekim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t i;
+
+	i = 0;
+	if (size == 0)
+	{
+		while (src[i])
+			i++;
+		return (i);
+	}
+	while (i < size - 1 && src[i] != '\0')
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	if (i < size)
+		dst[i] = '\0';
+	while (src[i] != '\0')
+		i++;
+	return (i);
+}
